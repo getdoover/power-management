b89f8404e2ff4380798102ec0d5947a1f34ed997
mathsman5133 <mathsman5132@gmail.com>
2026-05-22 10:30:19 +1000

    add live tags

--- src/solar_power_management/app_tags.py
+++ src/solar_power_management/app_tags.py
@@ -3,16 +3,16 @@ from pydoover.tags import Tag, Tags
 
 class PowerManagerTags(Tags):
     # Core measurements
-    system_voltage = Tag("number", default=None)
-    system_temperature = Tag("number", default=None)
+    system_voltage = Tag("number", default=None, live=True)
+    system_temperature = Tag("number", default=None, live=True)
     is_online = Tag("boolean", default=True)
 
     # Victron charger
     victron_hidden = Tag("boolean", default=True)
     charge_state = Tag("string", default=None)
-    charge_current = Tag("number", default=None)
-    charge_voltage = Tag("number", default=None)
-    charge_power = Tag("number", default=None)
+    charge_current = Tag("number", default=None, live=True)
+    charge_voltage = Tag("number", default=None, live=True)
+    charge_power = Tag("number", default=None, live=True)
 
     # Warning states
     low_battery_warning_sent = Tag("boolean", default=False)
